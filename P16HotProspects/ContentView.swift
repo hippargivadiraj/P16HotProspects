e291042c6869a4b3fbbe36748a6f9f2d55c7a2a5
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-08-30 14:32:32 -0700

    Initial Commit

---
+++ P16HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P16HotProspects
+//
+//  Created by Vadiraj Hippargi on 8/30/20.
+//  Copyright © 2020 Vadiraj Hippargi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
